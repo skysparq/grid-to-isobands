cc6da91a73c057ba59f9be8d94770f5c277b28fd
Tom Larsen <larsenthomasj@gmail.com>
2025-12-29 21:20:37 -0500

    seems to be working for sub-hemisphere polygons

--- isobands.R
+++ isobands.R
@@ -7,39 +7,15 @@ only_polys <- function(geometry) {
     filter(st_geometry_type(.) %in% c("POLYGON", "MULTIPOLYGON"))
 }
 
-flip_large_polygons <- function(data, area_threshold = 2.5e14) {
-  for (i in 1:nrow(data)) {
-    geom <- st_geometry(data[i,])
-    area <- as.numeric(st_area(geom))
-    
-    if (area > area_threshold) {
-      # Use s2 to rebuild with correct orientation
-      st_geometry(data[i,]) <- geom %>%
-        s2::as_s2_geography() %>%
-        s2::s2_rebuild(s2_options(dimensions = "polygon")) %>%
-        st_as_sfc()
-    }
-  }
-  return(data)
-}
-
 process_isobands <- function(input_path, output_path, tolerance_meters = 100) {
+  options(s2_oriented = TRUE)
   cat("Reading GeoJSON...\n")
+  sf_use_s2(FALSE)
   data <- st_read(input_path, quiet = TRUE)
   
-  data <- flip_large_polygons(data)
-
   cat(sprintf("Read %d features\n", nrow(data)))
-
-  #cross_edge <- 
-  #  st_is_valid(data, reason = TRUE) |>
-  #  grepl(x = _, pattern = "edge .* crosses edge", ignore.case = TRUE)
   
-  #st_geometry(data)[cross_edge] <-
-  #  st_geometry(data)[cross_edge] |>  
-  #  as_s2_geography(check = FALSE) |>
-  #  s2_union() |>
-  #  st_as_sfc() 
+  sf_use_s2(TRUE)
   
   simplified <- st_simplify(data, preserveTopology = TRUE, dTolerance = tolerance_meters)
   simplified <- only_polys(simplified)
@@ -65,7 +41,7 @@ process_isobands <- function(input_path, output_path, tolerance_meters = 100) {
     current_level <- levels_without_holes[[level_key]]
     cat(sprintf("Punching level %d...\n", i))
     i <- i + 1
-
+    
     # Erase accumulated mask from current level
     if (!is.null(accumulated_mask)) {
       tryCatch({
@@ -75,18 +51,18 @@ process_isobands <- function(input_path, output_path, tolerance_meters = 100) {
         cat(sprintf("  Warning: Error punching level %d: %s\n", level_idx, e$message))
       })
     }
-
+    
     # Remove empty geometries
     if (nrow(current_level) > 0) {
       current_level <- current_level[!st_is_empty(current_level), ]
     }
-
+    
     if (nrow(current_level) > 0) {
       final_results[[level_key]] <- lapply(1:nrow(current_level), function(i) {
-          st_cast(current_level[i, ], "POLYGON")
-        }) %>% do.call(rbind, .)
+        st_cast(current_level[i, ], "POLYGON")
+      }) %>% do.call(rbind, .)
     }
-
+    
     # Add to accumulated mask
     if (is.null(accumulated_mask)) {
       accumulated_mask <- st_make_valid(st_union(current_level))
@@ -95,15 +71,15 @@ process_isobands <- function(input_path, output_path, tolerance_meters = 100) {
       accumulated_mask <- st_make_valid(st_union(accumulated_mask, current_union))
     }
   }
-
+  
   # Combine results
   if (length(final_results) > 0) {
     all_features <- do.call(rbind, final_results)
-
+    
     cat(sprintf("Complete: %d features\n", nrow(all_features)))
-
+    
     st_write(all_features, output_path, delete_dsn = TRUE, quiet = TRUE)
-
+    
     cat(sprintf("Wrote output to %s\n", output_path))
   } else {
     cat("No valid features to write\n")
